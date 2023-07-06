b92236965fe21dd12da4c198e80419bbe0279f7e
yang20002020 <2628636987@qq.com>
2023-07-06 22:22:42 +0800

    mybatisplus 快速入门

---
+++ mybatisplus_001_quickstart/src/main/java/com/fuyu/domain/User.java
@@ -0,0 +1,60 @@
+package com.fuyu.domain;
+
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+    private String age;
+    private String tel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", age='" + age + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+}
---
+++ mybatisplus_001_quickstart/src/test/java/com/fuyu/Mybatisplus001QuickstartApplicationTests.java
@@ -0,0 +1,21 @@
+package com.fuyu;
+import com.fuyu.dao.UserDao;
+import com.fuyu.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+
+@SpringBootTest
+class Mybatisplus001QuickstartApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    void testGetAll() {
+        List<User> users = userDao.selectList(null);
+        System.out.println(users);
+    }
+
+}
