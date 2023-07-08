46675ddac4e426ea8f0e6a0a862df361bc1af096
yang20002020 <2628636987@qq.com>
2023-07-09 06:05:26 +0800

    全局配置

---
+++ mybatisplus_013_dml/src/main/java/com/fuyu/MybatisPlus013DqlApplication.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlus013DqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlus013DqlApplication.class, args);
+    }
+
+}
