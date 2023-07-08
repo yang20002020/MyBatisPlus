67417e2fb270e2dd1f3c15dbe3de990c3ab0505d
yang20002020 <2628636987@qq.com>
2023-07-09 05:24:48 +0800

    id生成策略 @TableId(type=  IdType.ASSIGN_ID)

---
+++ mybatisplus_012_dml/src/main/java/com/fuyu/MybatisPlus012DqlApplication.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlus012DqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlus012DqlApplication.class, args);
+    }
+
+}
---
+++ mybatisplus_012_dml/src/main/java/com/fuyu/query/UserQuery.java
@@ -0,0 +1,8 @@
+package com.fuyu.query;
+import com.fuyu.domain.User;
+import lombok.Data;
+
+@Data
+public class UserQuery extends User {
+    private Integer age2;
+}
