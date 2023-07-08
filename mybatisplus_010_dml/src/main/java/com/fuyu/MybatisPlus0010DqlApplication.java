7933f38817749cbccddb2ffaba1d1f708b9de4fa
yang20002020 <2628636987@qq.com>
2023-07-08 22:26:54 +0800

    id生成策略 @TableId(type= IdType.AUTO)

---
+++ mybatisplus_010_dml/src/main/java/com/fuyu/MybatisPlus0010DqlApplication.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlus0010DqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlus0010DqlApplication.class, args);
+    }
+
+}
