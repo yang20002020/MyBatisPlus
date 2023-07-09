26a16e0b10b64af804f241616d05d7b825ea3907
yang20002020 <2628636987@qq.com>
2023-07-09 08:39:00 +0800

    逻辑删除

---
+++ mybatisplus_015_dml/src/test/java/com/fuyu/MybatisPlus015DqlApplicationTests.java
@@ -0,0 +1,42 @@
+package com.fuyu;
+
+import com.fuyu.dao.UserDao;
+import com.fuyu.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+class MybatisPlus015DqlApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    void test() {
+        List<User> users = userDao.selectList(null);
+        System.out.println(users);
+    }
+
+    @Test
+    void testSave() {
+        User user = new User();
+        user.setName("程序员黑马");
+        user.setPassword("itheima");
+        user.setAge(12);
+        user.setTel("4006184000");
+        userDao.insert(user);
+    }
+
+    //测试逻辑删除
+    @Test
+    void testDelete() {
+        userDao.deleteById(1l);
+    }
+
+
+
+}
