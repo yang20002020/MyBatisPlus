bc7652d093a6bd89b5fd8d803b56c330ae40f1ec
yang20002020 <2628636987@qq.com>
2023-07-09 09:13:04 +0800

    逻辑删除

---
+++ mybatisplus_016_dml/src/main/java/com/fuyu/domain/User.java
@@ -0,0 +1,23 @@
+package com.fuyu.domain;
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+
+// @TableId(type= IdType.ASSIGN_ID)
+@Data
+//@TableName("tbl_user")
+public class User {
+   // @TableId(type= IdType.ASSIGN_ID)
+    private Long id;
+    private String name;
+    @TableField(value = "pwd",select = false)
+    private String password;
+    private Integer age;
+    private String tel;
+    @TableField(exist = false)
+    private Integer online;
+
+    //逻辑删除字段，标记当前记录是否被删除
+   // @TableLogic(value="0",delval = "1")
+    private  Integer deleted;
+}
