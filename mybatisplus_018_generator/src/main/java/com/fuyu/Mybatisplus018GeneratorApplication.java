21ceafd3e17994bc54471c51c5ebd6f85e348ed5
yang20002020 <2628636987@qq.com>
2023-07-09 23:48:09 +0800

    代码生成器

---
+++ mybatisplus_018_generator/src/main/java/com/fuyu/Mybatisplus018GeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fuyu.mybatisplus_018_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatisplus018GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatisplus018GeneratorApplication.class, args);
+    }
+
+}
